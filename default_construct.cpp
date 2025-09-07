b6959c229d9d4329e60fe5693dfc14c84deb3350
vijayydav20 <vijayyadav70254@gmail.com>
2025-09-07 21:34:53 +0530

    Add files via upload

---
+++ default_construct.cpp
@@ -0,0 +1,16 @@
+//ptogram for default constructor
+#include<iostream.h>
+class sum{
+	int a,b,c;
+	public:
+	sum(){
+	a=2;
+	b=3;
+	c=a+b;
+	cout<<"sum="<<c;	}
+};
+int main(){
+	sum obj;
+	
+};	
+	
