b32dc0e881e64dde2ce6a031c4b0d29f504dcc9a
vijayydav20 <vijayyadav70254@gmail.com>
2025-09-07 21:33:11 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,15 @@
+// program for class
+#include<iostream.h>
+class sum{
+	int a,b,c;
+	public:
+	void show(){
+	a=2;
+	b=3;
+	c=a+b;
+	cout<<"sum="<<c;	}
+};
+int main(){
+	sum obj;
+	obj.show();
+};	
